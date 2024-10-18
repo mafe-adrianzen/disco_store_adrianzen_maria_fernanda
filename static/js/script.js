19117134638178af794cfff1d281e35717ce1225
maria fernanda <a_madrianzen@colegiosanlorenzo.org>
2024-10-18 16:47:33 -0300

    first commit

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+
+// elimina boton
+
+function eliminarBoton() {
+  var boton = document.querySelector("#boton");
+  console.log("Este es el boton: " + boton);
+  // imprime el contenido del boton
+
+  boton.remove();
+  // la funcion remove() elimina un elemento
+
+}
+
